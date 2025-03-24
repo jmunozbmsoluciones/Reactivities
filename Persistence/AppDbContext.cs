70f9da310baee60783850025b6d69aeae5631967
jmunozbmsoluciones <jmunoz@bmsoluciones.cl>
2025-03-24 16:16:17 -0300

    End of section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+
+    public DbSet<Activity> Activities { get; set; }
+
+   
+
+}
